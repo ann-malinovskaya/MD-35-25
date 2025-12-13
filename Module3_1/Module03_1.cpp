68a86be5ebba4b016d7ee4c375d0bab15e4db6da
Ann Malinovskaya <ann.ivanova.7@gmail.com>
2025-12-13 23:55:58 +0300

    Module3_1

---
+++ Module3_1/Module03_1.cpp
@@ -0,0 +1,47 @@
+// Module03_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include <locale>
+#include <clocale>
+
+enum class TrafficLight { RED, YELLOW, GREEN };
+
+void printLight(TrafficLight light)
+{
+    switch (light)
+    {
+    case TrafficLight::RED:     std::cout << "Red" << std::endl; break;
+    case TrafficLight::YELLOW:  std::cout << "Yellow" << std::endl; break;
+    case TrafficLight::GREEN:   std::cout << "Green" << std::endl; break;
+    }
+}
+
+int main()
+{
+    std::setlocale(LC_ALL, "");
+
+    TrafficLight light = TrafficLight::RED;
+    printLight(light);
+
+    std::string str;
+    std::cout << "Введите цвет (red, yellow, green): " << std::endl;
+    std::cin >> str;
+
+    if (str == "red")
+        light = TrafficLight::RED;
+    else if (str == "yellow")
+        light = TrafficLight::YELLOW;
+    else if (str == "green")
+        light = TrafficLight::GREEN;
+    else
+    {
+        std::cout << "Некорректный цвет" << std::endl;
+        return 0;
+    }
+
+    printLight(light);
+}
+
+
